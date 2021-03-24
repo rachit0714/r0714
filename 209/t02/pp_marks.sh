351a349081e6470cc969ce0e1c9d444c82f10fe5
rachit <rachit0714@gmail.com>
2021-03-24 17:25:51 -0400

    commit

--- 209/t02/pp_marks.sh
+++ 209/t02/pp_marks.sh
@@ -5,8 +5,14 @@ grade=`cat feedback.txt | tr -d [:blank:]`
 
 IFS='/' read -ra grArray <<< "$grade"
 
+
 score=${grArray[0]}
 max=${grArray[1]}
+
+for a in $grArray
+do
+	echo $a
+done
 
 echo $score
 echo $max
